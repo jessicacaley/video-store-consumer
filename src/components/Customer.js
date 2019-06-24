7cb3b8ed17a2129eb4e113f80d0badfa890443d1
Jessica <jessica.homet@gmail.com>
2019-06-24 16:37:35 -0700

    clean up scaffold for ease of understanding

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -3,20 +3,11 @@ import PropTypes from 'prop-types';
 
 const Customer = (props) => {
   const { id, name } = props;
-
-  // const onClicked = () => {
-  //   props.getThing(id, name);
-  // };
-
+  
   return (
     <section className="customer">
       <h3>{ name }</h3>
       <h4>{ id }</h4>
-      {/* <div className="select_this_thing">
-        <button className="select_thing_button" onClick={onClicked}>
-          Select Thing
-        </button>
-      </div> */}
     </section>
   );
 };
--- src/components/Library.js
+++ src/components/Library.js
@@ -3,19 +3,9 @@ import PropTypes from 'prop-types';
 
 const Library = (props) => {
   const { movies } = props;
-
-  // const onClicked = () => {
-  //   props.getThing(id, name);
-  // };
-
   return (
     <section className="library">
       <h3>{ movies.first }</h3>
-      {/* <div className="select_this_thing">
-        <button className="select_thing_button" onClick={onClicked}>
-          Select Thing
-        </button>
-      </div> */}
     </section>
   );
 };
