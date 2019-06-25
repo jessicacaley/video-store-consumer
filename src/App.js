befc22984745e42c9ee3fc3010f3a39b186fb6dd
Kate Sandler <kate@katekatekate.net>
2019-06-25 16:01:11 -0700

    Map over customers

--- src/App.js
+++ src/App.js
@@ -2,41 +2,70 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 // import Movie from './components/Movie';
 
 class App extends Component {
   constructor() {
     super();
 
-    this.state = { 
+    this.state = {
       movies: [],
-      customers: [],
+      customers: []
     };
   }
 
-
   componentDidMount = () => {
-    axios.get('http://localhost:3000/')
-    .then((response) => {
-      const movies = response.data.flatMap(movie => { return [{ ...movie }] });
+    axios
+      .get('http://localhost:3000/')
+      .then(response => {
+        const movies = response.data.flatMap(movie => {
+          return [{ ...movie }];
+        });
 
-      this.setState({ movies: movies });
-    })
-    .catch((error) => {
-      this.setState({ errorMessage: error.message });
-    });
-  }
-  
+        this.setState({ movies: movies });
+      })
+      .catch(error => {
+        this.setState({ errorMessage: error.message });
+      });
+
+    axios
+      .get('http://localhost:3000/customers')
+      .then(response => {
+        const customers = response.data.flatMap(customer => {
+          return [{ ...customer }];
+        });
+
+        this.setState({ customers: customers });
+      })
+      .catch(error => {
+        this.setState({ errorMessage: error.message });
+      });
+  };
+
   render() {
+    const customerList = this.state.customers.map(customer => {
+      console.log(customer);
+      return (
+        <Customer
+          key={customer.id}
+          name={customer.name}
+          id={customer.id}
+          movies_checked_out_count={customer.movies_checked_out_count}
+        />
+      );
+    });
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>{customerList}</div>
       </div>
     );
   }
